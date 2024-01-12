a2dbf8b21b50f88de4dcb6f985abd703fca40611
tangentstorm <michal.wallace@gmail.com>
2024-01-12 15:37:13 -0500

    app: record tokens and uids on auth

--- rogo/app.py
+++ rogo/app.py
@@ -5,7 +5,7 @@ import asyncio
 import quart
 import jwt as jwtlib
 
-from .database import query
+from . import database as db
 
 app = quart.Quart(__name__)
 ok = None
@@ -29,8 +29,8 @@ async def about():
 
 
 def list_challenges_data():
-    return query("""
-        select c.name, c.title,
+    return db.query("""
+        select c.id, c.name, c.title,
           (select count(*) from tests t
             where t.chid = c.rowid) as num_tests
         from challenges c""")
@@ -55,6 +55,31 @@ async def show_challenge(name):
     return await quart.render_template('challenge.html', data=data[0])
 
 
+async def uid_from_request():
+    f = await quart.request.form
+    if not (j := f.get('jwt')):
+        raise LookupError('no jwt given')
+    r = db.query('select uid from tokens where jwt=?', [j])
+    if not r: raise LookupError('unrecognized jwt')
+    return r[0]['uid']
+
+
+@app.route('/c/<name>/attempt', methods=['POST'])
+async def attempt_challenge(name):
+    if not (uid := (await uid_from_request())):
+        return "log in first.", 403   # TODO: pretty 403 error
+    try:
+        [row] = db.query('select id from challenges where name=?', [name])
+        chid = row['id']
+    except ValueError:
+        raise LookupError(f'invalid challenge: {name!r}')
+    code = random_string()
+    db.commit("""
+        insert into attempts (uid, chid, code) values (?, ?, ?)
+        """, [uid, chid, code])
+    return {'aid': code}
+
+
 @app.route('/login', methods=['GET'])
 async def get_login():
     return await quart.render_template('login.html')
@@ -77,14 +102,14 @@ def get_auth_pre():
     """
 
 
-def make_pre_token():
-    res = ''.join(random.choice(string.ascii_letters) for _ in range(32))
-    return make_pre_token() if res in queues else res
+def random_string(length=32):
+    res = ''.join(random.choice(string.ascii_letters) for _ in range(length))
+    return random_string() if res in queues else res
 
 
 @app.route('/auth/pre', methods=['POST'])
 def post_auth_pre():
-    pre = make_pre_token()
+    pre = random_string()
     queues[pre] = asyncio.Queue()
     return {'token': pre}
 
@@ -122,6 +147,18 @@ async def post_auth_success():
 
     data = {'uid': acc['sub'], 'eml': acc['email']}
     jwt = JWT_OBJ.encode(data, JWT_KEY, alg='RS256')
+
+    # TODO: move this to a real signup process
+    sid = 1  # TODO: validate server
+    # TODO: use real usernames
+    uid = db.commit("""
+      replace into users (sid, authid, username)
+      values (?, ?, ?)
+      """, [1, acc['sub'], acc['email']])
+    db.commit("insert into tokens (uid, jwt) values (?, ?)",
+              [uid, jwt])
+
+    # now tell jwt to the listening command line client
     try:
         q = queues[pre]
         print("queues[pre]:", q)
