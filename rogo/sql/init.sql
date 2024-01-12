230130d4f4d741fb3b63add52b55f529a48d6655
tangentstorm <michal.wallace@gmail.com>
2024-01-12 12:59:01 -0500

    init.sql: expliict primary key ids

--- rogo/sql/init.sql
+++ rogo/sql/init.sql
@@ -1,44 +1,49 @@
 -- sqlite schema for rogo (both client and server)
 
 create table meta (
-    key text not null unique,
+    key text primary key,
     val text);
 
 insert into meta values ('schema_version', '1.0');
 
 
 create table servers (
+    id integer primary key,
     url text not null unique,
     name text not null,
     info text not null);
 
-insert into servers values
+insert into servers (url, name, info) values
   ('https://rogo.tangentcode.com/', 'tangentcode',
    'The original rogo server at tangentcode.com');
 
 
 create table users (
-    sid integer not null references servers (rowid),
+    id integer primary key,
+    sid integer not null references servers,
     authid text, -- references external authentication provider (firebase)
     username text not null);
 
 
 create table tokens (
-    uid integer not null references users (rowid),
+    id integer primary key,
+    uid integer not null references users,
     jwt text not null,
     ts integer not null );
 
 
 create table challenges (
-    sid  integer not null references servers (rowid),
-    name  text unique,
+    id integer primary key,
+    sid  integer not null references servers,
+    name  text,
     title text,
-    url   text );
+    unique (sid, name));
 
 
 create table attempts (
-    uid integer not null references users (rowid),
-    chid integer not null references challenges (rowid),
+    id integer primary key,
+    uid integer not null references users,
+    chid integer not null references challenges,
     ts integer not null,
     hash text unique,
     done integer,
@@ -49,14 +54,16 @@ create table attempts (
 
 
 create table progress (
-    aid integer not null references attempts (rowid),
-    tid integer not null references tests (rowid),
+    id integer primary key,
+    aid integer not null references attempts,
+    tid integer not null references tests,
     ts integer not null,
     vcs_ref text);
 
 
 create table tests (
-    chid integer not null references challenges (rowid),
+    id integer primary key,
+    chid integer not null references challenges,
     name text not null,
     head text not null,
     body text not null,
