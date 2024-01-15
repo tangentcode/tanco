17e627f3b013942ddb5842d69edd51f0d15f7ad8
tangentstorm <michal.wallace@gmail.com>
2024-01-15 17:54:25 -0500

    extract login.m.js and firebase-cfg.m.js

---
+++ rogo/static/firebase-cfg.m.js
@@ -0,0 +1,8 @@
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyB186Sn82G_z-u38HWQq5-PuMvTIDa4SX8",
+  authDomain: "rogo-dev-4e99f.firebaseapp.com",
+  projectId: "rogo-dev-4e99f",
+  storageBucket: "rogo-dev-4e99f.appspot.com",
+  messagingSenderId: "212137834895",
+  appId: "1:212137834895:web:bcfcad4e186caef72273fa"};
---
+++ rogo/static/login.m.js
@@ -0,0 +1,38 @@
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import {
+  getAuth,
+  signInWithRedirect,
+  signOut as signOut0,
+  GithubAuthProvider,
+  GoogleAuthProvider,
+  getRedirectResult
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js"
+
+import { firebaseConfig } from "./firebase-cfg.m.js";
+
+const _app = initializeApp(firebaseConfig);
+const auth = getAuth();
+await auth.authStateReady();
+
+
+// encapsulate the important bits of firebase auth:
+function currentUser() { return auth.currentUser }
+function signOut() { return signOut0(auth) }
+
+async function attemptLogin(how, onLoginSuccess) {
+  let provider = null;
+  switch(how) {
+    // TODO: case 'github': provider = new GithubAuthProvider(); break;
+    case 'google': provider = new GoogleAuthProvider(); break;
+    default: console.log('no provider specified'); return; }
+  await signInWithRedirect(auth, provider);
+  await getRedirectResult(auth);
+  if (auth.currentUser) { onLoginSuccess() }
+  else { alert('login failed')}}
+
+export {
+  currentUser,
+  signOut,
+  attemptLogin };
